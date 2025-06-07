89619237e2cfded1797040b97ca25106428d24bb
SharovRroman <144895815+SharovRroman@users.noreply.github.com>
2025-06-07 22:34:48 +0300

    Create task1.cpp

---
+++ LB27/task1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdexcept>  
+using namespace std;
+
+int main(void) {
+    int a = 8, b = 0, c = 0;
+    cin >> b;
+
+    try {
+        
+        if (b == 0) {
+            throw runtime_error("Your input is not valid, you can't divide by zero.");
+        }
+        c = a / b;
+        cout << c << endl;
+    }
+    catch (const runtime_error& e) {
+        
+        cerr << e.what() << endl;
+    }
+
+    return 0;
+}
