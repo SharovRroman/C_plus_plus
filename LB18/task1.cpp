6097d861a02a3610bd8d993466bb9132445b0138
SharovRroman <144895815+SharovRroman@users.noreply.github.com>
2025-06-07 22:18:43 +0300

    Create task1.cpp

---
+++ LB18/task1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void increment(int &var) {           
+    var += 1; 
+}
+
+void increment(int &var, int value) {   
+    var += value; 
+}
+
+int main() {
+    int var = 0;                           
+    for (int i = 0; i < 10; i++) {
+        if (i % 2)
+            increment(var);
+        else
+            increment(var, i);
+    }
+    cout << var << endl; 
+    return 0;
+}
