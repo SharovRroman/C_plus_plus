533ed436373d60474d11a4a49811e3d374d333d1
SharovRroman <144895815+SharovRroman@users.noreply.github.com>
2025-06-07 22:32:39 +0300

    Create task1.cpp

---
+++ LB25/task1.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+
+class Tree {
+public:
+    virtual char getSymbol() = 0; 
+    virtual ~Tree() {}
+
+    void draw(int height) {
+        cout << "Drawing tree with symbol '" << getSymbol() << "' and height " << height << ":\n";
+        for (int i = 0; i < height; ++i) {
+            
+            for (int j = 0; j < height - i - 1; ++j)
+                cout << " ";
+            
+            cout << "/";
+            for (int j = 0; j < 2 * i; ++j)
+                cout << getSymbol();
+            cout << "\\" << endl;
+        }
+    }
+};
+
+
+class TreeStar : public Tree {
+public:
+    char getSymbol() override {
+        return '*';
+    }
+};
+
+
+class TreePlus : public Tree {
+public:
+    char getSymbol() override {
+        return '+';
+    }
+};
+
+
+class TreeAt : public Tree {
+public:
+    char getSymbol() override {
+        return '-';
+    }
+};
+
+int main() {
+    int height;
+    cout << "Enter the height of the trees: ";
+    cin >> height;
+
+   
+    Tree* trees[3];
+    trees[0] = new TreeStar();
+    trees[1] = new TreePlus();
+    trees[2] = new TreeAt();
+
+    for (int i = 0; i < 3; ++i) {
+        trees[i]->draw(height);
+        cout << endl;
+    }
+
+    
+    for (int i = 0; i < 3; ++i) {
+        delete trees[i];
+    }
+
+    return 0;
+}
