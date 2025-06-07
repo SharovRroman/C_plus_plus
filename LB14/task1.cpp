453e307e3316f6a8a037e380fd4e01216c3b9e54
SharovRroman <144895815+SharovRroman@users.noreply.github.com>
2025-06-07 22:12:58 +0300

    Create task1.cpp

---
+++ LB14/task1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    std::srand(std::time(0)); 
+    
+    int size;
+    std::cout << "Enter array size: ";
+    std::cin >> size;
+    
+    double* arr = new double[size];
+    
+    int positiveCount = 0, negativeCount = 0;
+    
+    std::cout << "Generated array: ";
+    for (int i = 0; i < size; i++) {
+        arr[i] = -2.0 + static_cast<double>(std::rand()) / RAND_MAX * 4.0; 
+        std::cout << arr[i] << " ";
+        
+        if (arr[i] > 0) positiveCount++;
+        else if (arr[i] < 0) negativeCount++;
+    }
+    std::cout << std::endl;
+    
+    std::cout << "Number of positive numbers: " << positiveCount << std::endl;
+    std::cout << "Number of negative numbers: " << negativeCount << std::endl;
+    
+    delete[] arr; 
+    
+    return 0;
+}
