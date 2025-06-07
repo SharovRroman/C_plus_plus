86c85b24a785af3e4d845ab7cc1784b5d94ba655
SharovRroman <144895815+SharovRroman@users.noreply.github.com>
2025-06-07 22:35:34 +0300

    Create task2.cpp

---
+++ LB27/task2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a = 0, b = 0, c = 0;
+    
+    cout << "Enter two integers (dividend and divisor):\n";
+    cin >> a;  
+    cin >> b;  
+
+    try {
+        if (b == 0) {
+            
+            throw "Your input is not valid, you can't divide by zero.";
+        }
+        c = a / b;  
+        cout << "Result: " << c << endl;  
+    }
+    catch (const char* msg) {
+        
+        cout << msg << endl;
+    }
+
+    return 0;
+}
