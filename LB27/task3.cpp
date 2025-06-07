bb05fc6ce1cc83372554c5a75dce19cd027832c1
SharovRroman <144895815+SharovRroman@users.noreply.github.com>
2025-06-07 22:36:25 +0300

    Create task3.cpp

---
+++ LB27/task3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+const int DivideByZero = 111;
+
+float internaldiv(float arg1, float arg2)
+{
+    if (arg2 == 0.0)
+        throw DivideByZero; 
+    return arg1 / arg2;
+}
+
+int main(void)
+{
+    float r, a, b;
+    while (cin >> a)
+    {
+        cin >> b;
+        try
+        {
+            r = internaldiv(a, b); 
+            cout << r << endl;     
+        }
+        catch (int e)
+        {
+            if (e == DivideByZero)
+                cout << "Your input is not valid. You can't divide by zero." << endl; 
+        }
+    }
+    return 0;
+}
